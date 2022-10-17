f271606f438ff794502279ae0b3e907eae7eb5b7
Max Taylor <maxbransontaylor@gmail.com>
2022-10-17 02:34:31 -0400

    added div that displays links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import githubLogo from "../github.svg";
 import linkedInLogo from "../linkedin.svg";
 import phoneLogo from "../phone.svg";
 import emailLogo from "../email.svg";
 function Footer() {
+  const [contactDetails, setContactDetails] = useState("");
+  function hoverHandler(e) {
+    e.preventDefault();
+    const link = e.target.parentElement;
+    let info = link.href;
+    if (info) {
+      info = info.replace("mailto:", "").replace("tel:", "");
+    }
+    setContactDetails(info);
+  }
   return (
-    <footer className="d-flex justify-content-center align-items-center">
-      <a href="https://github.com/maxbransontaylor" target="_blank">
+    <footer
+      onMouseLeave={() => setContactDetails("")}
+      className="d-flex justify-content-center align-items-center row flex-wrap"
+    >
+      <a
+        className="col-2"
+        onMouseOver={hoverHandler}
+        aria-details="contact-details"
+        href="https://github.com/maxbransontaylor"
+        target="_blank"
+      >
         <img src={githubLogo} className="footer-icon" />
       </a>
-      <a href="https://www.linkedin.com/in/maxbtaylor/" target="_blank">
+      <a
+        className="col-2"
+        onMouseOver={hoverHandler}
+        aria-details="contact-details"
+        href="https://www.linkedin.com/in/maxbtaylor/"
+        target="_blank"
+      >
         <img src={linkedInLogo} className="footer-icon" />
       </a>
-      <a href="mailto:maxbransontaylor@gmail.com">
+      <a
+        className="col-2"
+        onMouseOver={hoverHandler}
+        aria-details="contact-details"
+        href="mailto:maxbransontaylor@gmail.com"
+      >
         <img src={emailLogo} className="footer-icon" />
       </a>
-      <a href="tel:9195309428">
+      <a
+        className="col-2"
+        onMouseOver={hoverHandler}
+        aria-details="contact-details"
+        href="tel:9195309428"
+      >
         <img src={phoneLogo} className="footer-icon" />
       </a>
+      <div
+        id="contact-details"
+        className={`d-flex justify-content-center ${
+          contactDetails ? "yopacity" : "nopacity"
+        }`}
+      >
+        <h2>{contactDetails}</h2>
+      </div>
     </footer>
   );
 }
